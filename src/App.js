e6238e236a712ec0f7c71b7a19d4eb0251d23572
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-10-08 14:27:19 -0400

    boilerplate pages added with route animations using react spring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import { useTransition, animated } from 'react-spring'
 
+import useRouter from './useRouter'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+
+const Container = styled.div`
+  padding: 40px 0 20px 0;
+  margin: 0 5vw;
+  width: 90vw;
+  position: fixed;
+  top: 15vh;
+  display: flex;
+  justify-content: center;
+  z-index: -1;
+`
 
 function App() {
-  return (
-    <>
+  const { location } = useRouter()
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+  return transitions.map(({ item, props, key }) => (
+    <animated.div key={key} style={props}>
       <Navbar />
-    </>
-  )
+      <Container className="container">
+        <Switch location={item}>
+          <Route exact path="/" render={() => <Home />}/>
+          <Route path="/about" render={() => <About />}/>
+          <Route path="/contact" render={() => <Contact />}/>
+        </Switch>
+      </Container>
+    </animated.div>
+  ))
 }
 
 export default App
