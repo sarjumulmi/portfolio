cbe06c8f8d29056baaf04de97a7e5dfaefd6d623
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-24 14:45:16 +0000

    Deploy sarjumulmi/portfolio to github.com/sarjumulmi/portfolio.git:gh-pages

---
+++ precache-manifest.7e9b9e3a6a3f2cfb6d581e4e7291cd14.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935584ea6fdc498f3305654c34b84b7c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "428d56980a4aa4a2fbd6",
+    "url": "/portfolio/static/js/2.2e7ea3c4.chunk.js"
+  },
+  {
+    "revision": "468b8f1da8412cf4bb73",
+    "url": "/portfolio/static/js/main.5e03a128.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "02855f46f0b373437f7bc5af270da49b",
+    "url": "/portfolio/static/media/email.02855f46.svg"
+  },
+  {
+    "revision": "1ce9fdbc1a6ba345fa68d665dbf2e381",
+    "url": "/portfolio/static/media/facebook.1ce9fdbc.svg"
+  },
+  {
+    "revision": "21d620ef0ececdc58f216f04f20d915b",
+    "url": "/portfolio/static/media/github.21d620ef.svg"
+  },
+  {
+    "revision": "62f33dd3d486178292b9d36eb22ac7ec",
+    "url": "/portfolio/static/media/instagram.62f33dd3.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/portfolio/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "b67c626a345676a345186be74da7bac8",
+    "url": "/portfolio/static/media/unicorn.b67c626a.svg"
+  }
+]);
